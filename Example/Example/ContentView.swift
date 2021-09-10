188bd45669fcfd6ceb129c02506a0880db0a3632
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2021-09-10 15:03:53 +0600

    Added Example app

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  MIT License
+//
+//  Copyright (c) 2021 Tamerlan Satualdypov
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import SwiftUI
+import TabBar
+
+struct ContentView: View {
+    
+    private enum Item: Int, Tabbable {
+        case first = 0
+        case second
+        case third
+        
+        var icon: String {
+            switch self {
+                case .first: return "house"
+                case .second: return "magnifyingglass"
+                case .third: return "person"
+            }
+        }
+        
+        var title: String {
+            switch self {
+                case .first: return "First"
+                case .second: return "Second"
+                case .third: return "Third"
+            }
+        }
+    }
+    
+    @State private var selection: Item = .first
+    
+    var body: some View {
+        TabBar(selection: $selection) {
+            Text("First")
+                .tabItem(for: Item.first)
+            
+            Text("Second")
+                .tabItem(for: Item.second)
+            
+            Text("Third")
+                .tabItem(for: Item.third)
+        }
+        .tabBar(style: CustomTabBarStyle())
+        .tabItem(style: CustomTabItemStyle())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/Example/Styles/TabItem/CustomTabItemStyle.swift
@@ -0,0 +1,44 @@
+//
+//  MIT License
+//
+//  Copyright (c) 2021 Tamerlan Satualdypov
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import SwiftUI
+import TabBar
+
+struct CustomTabItemStyle: TabItemStyle {
+    
+    public func tabItem(icon: String, title: String, isSelected: Bool) -> some View {
+        ZStack {
+            if isSelected {
+                Circle()
+                    .foregroundColor(.blue)
+                    .frame(width: 40.0, height: 40.0)
+            }
+            
+            Image(systemName: icon)
+                .foregroundColor(isSelected ? .white : Color("color.tab.item.foreground"))
+                .frame(width: 32.0, height: 32.0)
+        }
+        .padding(.vertical, 8.0)
+    }
+    
+}
