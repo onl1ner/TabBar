bbfd0f45f53213832ab57aa34a9e4c61e51a1368
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2023-01-22 15:00:12 +0600

    Added a parameter for providing selected icon name

--- Sources/TabBar/Common/Protocol/Tabbable.swift
+++ Sources/TabBar/Common/Protocol/Tabbable.swift
@@ -27,9 +27,18 @@ import Foundation
  A type that represents an item of your `TabBar` component.
  */
 public protocol Tabbable: Hashable {
-    /// Icon of `TabBar's` item.
+    /// Icon name of `TabBar's` item.
     var icon: String { get }
+    
+    /// Selected icon name of `TabBar's` item.
+    var selectedIcon: String { get }
     
     /// Title of `TabBar's` item.
     var title: String { get }
+}
+
+public extension Tabbable {
+    var selectedIcon: String {
+        return self.icon
+    }
 }
